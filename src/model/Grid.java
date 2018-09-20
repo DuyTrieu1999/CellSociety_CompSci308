46869d5c664f9e97d08f3c11fb16e43ca35dea36
Samuel Appiah-Kubi <sa285@duke.edu>
2018-09-20 01:27:39 -0400

    update method for game of life in Grid

--- src/model/Grid.java
+++ src/model/Grid.java
@@ -6,30 +6,50 @@ import java.util.ResourceBundle;
 
 import javafx.scene.layout.Pane;
 
-public abstract class Grid {
+public class Grid {
 
     private Cell[][] grid;
     private int rowLength = 20;
-    private int colLength = 15;
+    private int colLength = 20;
     private double xPos;
     private double yPos;
 
     public Grid () {
         grid = new Cell[rowLength][colLength];
     }
-    public void fillGrid (Pane pane) {
+    public void fillGrid (String cellName) {
         for (int i = 0; i<this.getRowNum(); i++) {
             for (int j = 0; j<this.getColNum(); j++) {
-                Cell cell = grid[i][j];
+                grid[i][j] = returnCell(cellName, i, j);
                 //TODO: Add in rules and stuffs
             }
         }
     }
+
+    public void updateCells() {
+        for (int i = 0; i<this.getRowNum(); i++) {
+            for (int j = 0; j<this.getColNum(); j++) {
+                int neighborSum = 0;
+                for(int x = -1; x < 1; x++) {
+                    for (int y = -1; y < 1; y++) {
+                        grid[i + x][j + y].setPrevState(grid[i + x][j + y].getCurrState());
+                        neighborSum += grid[i + x][j + y].getCurrState();
+                    }
+                }
+                neighborSum -= grid[i][j].getCurrState();
+                if      ((grid[i][j].getCurrState() == 1) && (neighborSum <  2)) grid[i][j].setCurrState(0);
+                else if ((grid[i][j].getCurrState() == 1) && (neighborSum >  3)) grid[i][j].setCurrState(0);
+                else if ((grid[i][j].getCurrState() == 0) && (neighborSum == 3)) grid[i][j].setCurrState(1);
+                else grid[i][j].setCurrState(grid[i][j].getPrevState());
+            }
+
+        }
+    }
     private boolean rowOutOfBound (int row) {
-        return row < 0 || row > getRowNum();
+        return row < 0 || row > getRowNum() - 1;
     }
     private boolean colOutOfBound (int col) {
-        return col < 0 || col > getColNum();
+        return col < 0 || col > getColNum() - 1;
     }
     private int getRowNum () {
         return grid.length;
@@ -43,4 +63,11 @@ public abstract class Grid {
     private Cell getCell (int row, int col) {
         return grid[row][col];
     }
+
+    private Cell returnCell(String cellName, int row, int col) {
+        if(cellName.equals("fire"))
+            return new FireCell(row,col);
+        return null;
+    }
+
 }
