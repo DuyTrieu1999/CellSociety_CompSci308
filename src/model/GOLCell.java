189e26b2b5b4f164d47a21b717aafa013a54c12d
austkao <nitsuaoak@outlook.com>
2018-09-20 23:19:56 -0400

    Implemented Game of Life

--- src/model/GOLCell.java
+++ src/model/GOLCell.java
@@ -1,7 +1,10 @@
 package model;
 
 import java.util.*;
-
+/**
+ * This class implements a cell that would be part of the Game of Life simulation. Most of the initial testing should be with this simulation.
+ * @author Austin Kao
+ */
 public class GOLCell extends Cell{
     private int numAlive;
 
@@ -12,7 +15,18 @@ public class GOLCell extends Cell{
     public GOLCell updateCell(GOLCell cell) {
         ArrayList<GOLCell>() currNeighbors = cell.getNeighbors();
         for(GOLCell neighbor : currNeighbors) {
-            if(neighbor.get)
+            if(neighbor.getCurrState.equals("Alive")) {
+                numAlive++;
+            }
+        }
+        if(this.getCurrState.equals("Alive") && (numAlive < 1 || numAlive > 3)) {
+            this.setPrevState("Alive");
+            this.setCurrState("Dead");
+        } else if(this.getCurrState.equals("Dead") && numAlive == 3) {
+            this.setPrevState("Dead");
+            this.setCurrState("Alive");
+        } else {
+            this.setPrevState(this.getCurrState());
         }
     }
 }
