75c99c42d244b91eeb4406542452bf33fd054f41
austkao <nitsuaoak@outlook.com>
2018-09-21 23:22:53 -0400

    Added Spreading Fire simulation

--- src/model/FireCell.java
+++ src/model/FireCell.java
@@ -3,9 +3,22 @@ package model;
 import javafx.scene.paint.Color;
 
 import java.util.Map;
+import java.util.Random;
 
 public class FireCell extends Cell {
+    private double probCatch;
+
     public FireCell (int row, int col, double width) {
         super(row, col, width);
+    }
+
+    public void updateCell() {
+        if(this.getCurrState() == 1) {
+            double rn = Math.random();
+            this.setPrevState(1);
+            if(rn < probCatch) {
+                this.setCurrState(2);
+            }
+        }
     }
 }
--- src/model/PredatorPreyCell.java
+++ src/model/PredatorPreyCell.java
@@ -1,6 +1,17 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.Map;
+
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+/**
+ * This class implements the Wa-Tor World of predator-prey relationships model. **Requires research?
+ */
+
 public class PredatorPreyCell extends Cell {
+    //Temporary parameters?
     private final int KELP = 0;
     private final int FISH = 1;
     private final int SHARK = 2;
