16610cbb8c76ff2d107a4db91c2fac7a371ddcec
dvt5@duke.edu <dvt5@duke.edu>
2018-09-22 01:50:31 -0400

    adding drop down rules to update simulation

--- src/view/SimulationUI.java
+++ src/view/SimulationUI.java
@@ -54,8 +54,9 @@ public class SimulationUI {
         myRoot = new Group();
         myScene = new Scene(myRoot, SceneENUM.SCENE_WIDTH.getVal(), SceneENUM.SCENE_HEIGHT.getVal(), BACKGROUND);
         makeAllButton();
-        myGrid = new Grid();
+        myGrid = new Grid("Game of Life");
         addGridPane(myGrid);
+        myRoot.getChildren().add(myGridPane);
 //        myScene.getStylesheets().add("./view/SimulationUIStyle.css");
         return myScene;
     }
@@ -112,11 +113,12 @@ public class SimulationUI {
     }
 
     private ChoiceBox makeChoiceBox () {
-        ChoiceBox<String> cb = new ChoiceBox<String>();
+        ChoiceBox<String> cb = new ChoiceBox<>();
+        cb.getItems().add("Game of Life");
         cb.getItems().add("Wa-Tor World model");
         cb.getItems().add("Spreading of Fire");
         cb.getItems().add("Schelling's model of segregation");
-        cb.setValue("Wa-Tor World model");
+        cb.setValue("Game of Life");
         cb.setOnAction(e -> getChoice(cb));
         return cb;
     }
@@ -124,6 +126,10 @@ public class SimulationUI {
     private void getChoice(ChoiceBox<String> cb) {
         String name = cb.getValue();
         System.out.println(name);
+        if (name.equals("Game of Life")) {
+            myRoot.getChildren().remove(myGridPane);
+            System.out.println(myGrid);
+        }
     }
 
     private void setDimensions(SimuButton btn) {
@@ -177,7 +183,6 @@ public class SimulationUI {
         }
         addCellToGrid();
         myGridPane.setPadding(new Insets(60,60,60,50));
-        myRoot.getChildren().add(myGridPane);
     }
     private void addCellToGrid () {
         for (int i=0; i<myGrid.getRowNum();i++) {
